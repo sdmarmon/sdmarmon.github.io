6bb962df7336efa42eb6b84016b414c465d619e4
sdmarmon <sylvain.duhau.marmon@gmail.com>
2024-07-16 00:35:14 +0200

    Trapping focus inside the mobileNavOverlay

--- includes/input.js
+++ includes/input.js
@@ -1,9 +1,11 @@
 import Alpine from 'alpinejs'
 import navigationMenu from './navigationMenu.js'
 import projectCards from './projectCards.js'
+import focus from '@alpinejs/focus'
 
 Alpine.data('navigationMenu', navigationMenu)
 Alpine.data('projectCards', projectCards)
+Alpine.plugin(focus)
 
 window.Alpine = Alpine
 Alpine.start()
