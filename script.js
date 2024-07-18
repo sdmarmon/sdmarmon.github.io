d93330118665e5dcd431ccc59f5293e17dbd0c3e
sdmarmon <sylvain.duhau.marmon@gmail.com>
2024-07-18 23:33:12 +0200

    Adding a site map

--- script.js
+++ script.js
@@ -3262,12 +3262,12 @@ ${expression ? 'Expression: "' + expression + '"\n\n' : ""}`, el);
   // includes/navigationMenu.js
   var navigationMenu_default = () => ({
     menus: [
-      { title: "About me", icon: "icon-user", link: "#about", newTab: false },
-      { title: "Portfolio", icon: "icon-briefcase", link: "#portfolio", newTab: false },
-      { title: "Contact", icon: "icon-mail-alt", link: "#contact", newTab: false },
-      { title: "GitHub", icon: "icon-github-circled", link: "https://github.com/sdmarmon", newTab: true },
-      { title: "LinkedIn", icon: "icon-linkedin-squared", link: "https://www.linkedin.com/in/sylvain-d-marmon", newTab: true },
-      { title: "Resume", icon: "icon-file-pdf", link: "assets/Resume_Duhau-Marmon.pdf", newTab: true }
+      { title: "About me", icon: "icon-user", link: "#about", newTab: false, ext: false },
+      { title: "Portfolio", icon: "icon-briefcase", link: "#portfolio", newTab: false, ext: false },
+      { title: "Contact", icon: "icon-mail-alt", link: "#contact", newTab: false, ext: false },
+      { title: "GitHub", icon: "icon-github-circled", link: "https://github.com/sdmarmon", newTab: true, ext: true },
+      { title: "LinkedIn", icon: "icon-linkedin-squared", link: "https://www.linkedin.com/in/sylvain-d-marmon", newTab: true, ext: true },
+      { title: "Resume", icon: "icon-file-pdf", link: "assets/Resume_Duhau-Marmon.pdf", newTab: true, ext: false }
     ]
   });
 
